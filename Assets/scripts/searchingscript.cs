903550196e01e192763f2616bf60ec9aee1c20c9
perezzil <perezzil@OA-S141-14>
2026-01-14 11:08:29 -0500

    did coding gym 7, made a script that moves a sprite from its starting position to a randomized second position using lerp

---
+++ Assets/scripts/searchingscript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class searchingscript : MonoBehaviour
+{
+
+    public Vector2 start;
+
+    public Vector2 end;
+
+    public float t = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        start = transform.position;
+
+        end.x = Random.Range(-3, 3);
+        end.y = Random.Range(-3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        if (t > 1)
+        {
+            t = 0;
+        }
+
+        transform.position = Vector2.Lerp(start, end, t);
+    }
+}
